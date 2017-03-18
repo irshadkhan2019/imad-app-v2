4e4add3ae0f527abd10a44dcdbc079c918a9d601
irshadkhan2019 <khanirshad2019@gmail.com>
2017-03-18 16:39:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/Article-one', function (req, res) {
+app.get('/article-one', function (req, res) {
   res.sendFile('Article 1 is requested and it will bve served');
 });
 
-app.get('/Article-two', function (req, res) {
+app.get('/article-two', function (req, res) {
   res.sendFile('Article 2 is requested and it will bve served');
 });
 
-app.get('/Article-three', function (req, res) {
+app.get('/article-three', function (req, res) {
   res.sendFile('Article 3 is requested and it will bve served');
 });
 
